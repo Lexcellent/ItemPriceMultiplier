7536888f0308076fc806b9c3e62d3d2df6acc89e
Lexcellent <2587936056@qq.com>
2025-10-18 21:32:30 +0800

    显示出售价格

--- ItemPriceMultiplier/ModBehaviour.cs
+++ ItemPriceMultiplier/ModBehaviour.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 using HarmonyLib;
 using System.IO;
 using Duckov.Economy;
+using Duckov.UI;
+using Duckov.Utilities;
 using ItemStatsSystem;
+using TMPro;
 
 namespace ItemPriceMultiplier
 {
@@ -12,6 +15,7 @@ namespace ItemPriceMultiplier
         private bool _isInit = false;
         private string? _harmonyId = null;
         private static int _itemPriceMultiplier = 2;
+        private static bool _showPrice = true;
 
         protected override void OnAfterSetup()
         {
@@ -62,6 +66,15 @@ namespace ItemPriceMultiplier
                                 Debug.Log($"ItemPriceMultiplier模组：已从配置文件读取ItemPriceMultiplier值: {multiplier}");
                             }
                         }
+                        else if (line.StartsWith("ShowPrice="))
+                        {
+                            string value = line.Substring("ShowPrice=".Length).Trim();
+                            if (bool.TryParse(value, out bool showPrice))
+                            {
+                                _showPrice = showPrice;
+                                Debug.Log($"ItemPriceMultiplier模组：已从配置文件读取ShowPrice值: {showPrice}");
+                            }
+                        }
                     }
                 }
                 else
@@ -90,6 +103,60 @@ namespace ItemPriceMultiplier
             {
                 Debug.Log($"ItemPriceMultiplier模组：错误：{e.Message}");
             }
+        }
+
+        TextMeshProUGUI? _text = null;
+
+        TextMeshProUGUI Text
+        {
+            get
+            {
+                if (_text == null)
+                {
+                    _text = Instantiate(GameplayDataSettings.UIStyle.TemplateTextUGUI);
+                }
+
+                return _text;
+            }
+        }
+
+        void Awake()
+        {
+            Debug.Log("DisplayItemValue Loaded!!!");
+        }
+
+        void OnDestroy()
+        {
+            if (_text != null)
+                Destroy(_text);
+        }
+
+        void OnEnable()
+        {
+            ItemHoveringUI.onSetupItem += OnSetupItemHoveringUI;
+        }
+
+        void OnDisable()
+        {
+            ItemHoveringUI.onSetupItem -= OnSetupItemHoveringUI;
+        }
+
+        private void OnSetupItemHoveringUI(ItemHoveringUI uiInstance, Item item)
+        {
+            if (!_showPrice || item == null)
+            {
+                Text.gameObject.SetActive(false);
+                return;
+            }
+
+            Text.gameObject.SetActive(true);
+            Text.transform.SetParent(uiInstance.LayoutParent);
+            Text.transform.localScale = Vector3.one;
+            float num = 0.5f;
+            var convertPrice = Mathf.FloorToInt((float)item.GetTotalRawValue() * num);
+            Text.text = $"${convertPrice * _itemPriceMultiplier}";
+            Text.fontSize = 20f;
+            Text.color = Color.green;
         }
     }
 }
